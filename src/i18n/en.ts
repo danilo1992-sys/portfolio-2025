feeb46d850ef88bce1d442fe27a8c83e60bf2942
danilo1992-sys <sanfracaceresdanilo@gmail.com>
2025-05-14 17:18:42 -0300

    Add: modificando archivo de traduccion

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -14,6 +14,17 @@ export default {
   home:{
     about: "Danilo Caceres full stack programmer with",
     and:"and",
+  },
+
+  experience:{
+    its: "Creation of a web page with backend and frontend for a dog hotel. Frontend created with HTML5 and CSS. Backend created in Bash script and connection to a MySQL database.",
+    titulo1: "System support",
+    python: "Creation of several desktop applications with Python and TKinter, and creation of web applications in Flask",
+    titulo2: "Python flask programmer",
+    javascript: "Creation of fullstack web applications with react and node",
+    titulo3: "Javascript programmer with node",
+    docker: "Infrastructure with docker",
+    titulo4: "Creation of infrastructure with docker and kubernetes",
   }
 }
 
--- src/i18n/es.ts
+++ src/i18n/es.ts
@@ -14,5 +14,16 @@ export default {
   home:{
     about:"Danilo Caceres porgramador full stack",
     and:"con",
+  },
+
+  experience:{
+    its:"Creación de una página web con backend y frontend para un hotel canino. Frontend creado con HTML5 y CSS. Backend creado en Bash script y conexión a una base de datos MySQL.",
+    titulo1:"Soporte de sistemas",
+    python:"Creación de varias aplicaciones de escritorio con Python y TKinter, y creación de aplicaciones web en Flask",
+    titulo2:"Programador python flask",
+    javascript:"Creación de aplicaciones web fullstack con react y node",
+    titulo3:"Programador javascrip con node",
+    docker:"Infraestructura con docker",
+    titulo4:"Creacion de infraestrucutura con docker y kubernetes",
   }
 }
